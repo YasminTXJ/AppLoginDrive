752f2d1233a0e64b3582b7961626d90a17330bcc
YasminTXJ <yasminteixeiradejesus@gmail.com>
2024-10-13 13:07:44 -0300

    upload funcionando

--- app/src/main/java/com/example/applogindrive/PrincipalActivity.kt
+++ app/src/main/java/com/example/applogindrive/PrincipalActivity.kt
@@ -1,6 +1,5 @@
 package com.example.applogindrive
 
-
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
@@ -12,35 +11,34 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.auth.GoogleAuthException
-import com.google.android.gms.auth.GoogleAuthUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.Scope
 import com.google.api.client.extensions.android.http.AndroidHttp
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
-import com.google.api.client.http.HttpRequestInitializer
 import com.google.api.client.http.InputStreamContent
-import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
-
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
 import com.google.api.services.drive.model.File
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.io.InputStream
+
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var googleDriveService: Drive
     private lateinit var textFileName: TextView
     private var fileUri: Uri? = null
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private lateinit var driveService: Drive
     private lateinit var googleAccount: GoogleSignInAccount
     private val context: Context get() = this
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,17 +48,16 @@ class PrincipalActivity : AppCompatActivity() {
         buttonSignOut.setOnClickListener {
             signOut()
         }
-
-
+        createGoogleSignInClient()
         // Recupera a conta passada pela MainActivity
         googleAccount = intent.getParcelableExtra("account")
             ?: throw IllegalArgumentException("Conta Google não recebida")
 
-
+        // Inicializa o serviço do Google Drive em uma coroutine
         initializeDriveService()
+
         // Exemplo de uso do Drive: Mostrar informações da conta logada
         displayAccountInfo()
-
 
         val buttonSelectFile: Button = findViewById(R.id.button_select_file)
         val buttonUploadFile: Button = findViewById(R.id.button_upload_file)
@@ -73,8 +70,6 @@ class PrincipalActivity : AppCompatActivity() {
         buttonUploadFile.setOnClickListener {
             uploadFileToDrive()
         }
-
-
     }
 
     private fun openFileChooser() {
@@ -95,19 +90,18 @@ class PrincipalActivity : AppCompatActivity() {
 
     // Inicializa o serviço do Google Drive utilizando a conta Google recebida
     private fun initializeDriveService() {
-        try {
-            Toast.makeText(this, "email ${googleAccount.account}", Toast.LENGTH_SHORT).show()
-            Log.d("PrincipalActivity", "Conta ${googleAccount.account}")
-            driveService = initializeDrive(googleAccount)
-            Log.d("PrincipalActivity", "Drive API inicializado com sucesso.")
-        } catch (e: GoogleAuthException) {
-            Log.e("PrincipalActivity", "Erro de autenticação: ${e.message}")
-        } catch (e: IOException) {
-            Log.e("PrincipalActivity", "Erro de IO: ${e.message}")
-        } catch (e: Exception) {
-            Log.e("PrincipalActivity", "Erro geral: ${e.message}")
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                googleDriveService = initializeDrive(googleAccount)
+                Log.d("PrincipalActivity", "Drive API inicializado com sucesso.")
+            } catch (e: GoogleAuthException) {
+                Log.e("PrincipalActivity", "Erro de autenticação: ${e.message}")
+            } catch (e: IOException) {
+                Log.e("PrincipalActivity", "Erro de IO: ${e.message}")
+            } catch (e: Exception) {
+                Log.e("PrincipalActivity", "Erro geral: ${e.message}")
+            }
         }
-
     }
 
     // Exemplo simples de exibir informações da conta logada
@@ -118,29 +112,36 @@ class PrincipalActivity : AppCompatActivity() {
 
     private fun uploadFileToDrive() {
         fileUri?.let { uri ->
-            try {
-                val inputStream: InputStream? = contentResolver.openInputStream(uri)
-                val fileName = uri.lastPathSegment ?: "Arquivo sem nome"
+            lifecycleScope.launch(Dispatchers.IO) {
+                try {
+                    val inputStream: InputStream? = contentResolver.openInputStream(uri)
+                    val fileName = uri.lastPathSegment ?: "Arquivo sem nome"
 
-                val fileMetadata = File()
-                fileMetadata.name = fileName
+                    val fileMetadata = File().apply {
+                        name = fileName
+                    }
 
-                val file =  driveService.files().create(
-                    fileMetadata,
-                    InputStreamContent("application/octet-stream", inputStream)
-                )
-                    .setFields("id")
-                    .execute()
-                Log.d("PrincipalActivity", "Arquivo enviado com ID: ${file.id}")
-                Toast.makeText(this, "Arquivo enviado com sucesso!", Toast.LENGTH_SHORT).show()
-            } catch (e: IOException) {
-                Log.e("PrincipalActivity", "Erro ao enviar arquivo", e)
-                Toast.makeText(this, "Erro ao enviar arquivo: ${e.message}", Toast.LENGTH_SHORT)
-                    .show()
+                    val file = googleDriveService.files().create(
+                        fileMetadata,
+                        InputStreamContent("application/octet-stream", inputStream)
+                    )
+                        .setFields("id")
+                        .execute()
+
+                    Log.d("Drive", "Arquivo enviado com ID: ${file.id}")
+
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@PrincipalActivity, "Arquivo enviado com sucesso!", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: IOException) {
+                    Log.e("Drive", "Erro ao enviar arquivo", e)
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@PrincipalActivity, "Erro ao enviar arquivo: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
     }
-
 
     private fun signOut() {
         googleSignInClient.signOut().addOnCompleteListener(this) {
@@ -160,7 +161,7 @@ class PrincipalActivity : AppCompatActivity() {
     // Função para inicializar o Google Drive com a conta Google logada
     private fun initializeDrive(googleAccount: GoogleSignInAccount): Drive {
         val credential = GoogleAccountCredential.usingOAuth2(
-                context, listOf(DriveScopes.DRIVE, DriveScopes.DRIVE_FILE)
+            context, listOf(DriveScopes.DRIVE, DriveScopes.DRIVE_FILE)
         ).setSelectedAccount(googleAccount.account)
 
         return Drive.Builder(
@@ -171,8 +172,12 @@ class PrincipalActivity : AppCompatActivity() {
             .setApplicationName(context.getString(R.string.app_name))
             .build()
     }
+    private fun createGoogleSignInClient() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestScopes(Scope(DriveScopes.DRIVE_FILE), Scope(DriveScopes.DRIVE))
+            .build()
 
-
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
 }
-
-
